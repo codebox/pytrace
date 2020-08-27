bdc0f26b2585c593656c384cb2e36882e31504c5
Rob Dawson <rob@codebox.org.uk>
2020-08-27 17:35:46 +0100

    more classes

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from PIL import Image, ImageDraw
 
+class Screen:
+    def __init__(self, width, height, distance):
+        self.width = width
+        self.height = height
+        self.distance = distance
+
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
 class Scene:
-    def __init__(self, screen, background_colour = (0,0,0)):
-        self.screen_width, self.screen_height = screen
+    def __init__(self, screen, camera_position, background_colour):
+        self.screen = screen
+        self.camera_position = camera_position
         self.background_colour = background_colour
         self.objects = []
         self.lights = []
@@ -14,7 +27,7 @@ class Scene:
         self.lights.append(light)
 
     def render(self, output_file):
-        image = Image.new('RGB', (self.screen_width, self.screen_height), self.background_colour)
+        image = Image.new('RGB', (self.screen.width, self.screen.height), self.background_colour)
         image.save(output_file)
 
 
@@ -26,8 +39,17 @@ class Light:
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 200
+SCREEN_DISTANCE = 1000
+screen = Screen(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_DISTANCE)
 
-scene = Scene((SCREEN_WIDTH, SCREEN_HEIGHT))
+CAMERA_X = 0
+CAMERA_Y = 0
+CAMERA_Z = 0
+camera_position = Point(CAMERA_X, CAMERA_Y, CAMERA_Z)
+
+COLOUR_BLACK = (0, 0, 0)
+
+scene = Scene(screen, camera_position, COLOUR_BLACK)
 scene.add_object(Sphere())
 scene.add_light(Light())
 scene.render('output.png')
