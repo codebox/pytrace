574b222d5860df9a7f2391ebd349d804737684c7
Rob Dawson <rob@codebox.org.uk>
2020-08-29 19:48:05 +0100

    somehow broken, check maths

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import math
 from PIL import Image, ImageDraw
 
+
 class Screen:
     def __init__(self, width, height, distance):
         self.width = width
         self.height = height
         self.distance = distance
+
 
 class Point:
     def __init__(self, x, y, z):
@@ -13,12 +15,16 @@ class Point:
         self.y = y
         self.z = z
 
-    def distance_to(self, other):
-        return math.sqrt(
-            math.pow(self.x - other.x, 2) +
-            math.pow(self.y - other.y, 2) +
-            math.pow(self.z - other.z, 2)
+    def vector_to(self, other):
+        return Vector(
+            other.x - self.x,
+            other.y - self.y,
+            other.z - self.z
         )
+
+    def distance_to(self, other):
+        return self.vector_to(other).magnitude()
+
 
 class Vector:
     def __init__(self, x, y, z):
@@ -32,6 +38,24 @@ class Vector:
 
     def dot_product(self, other):
         return self.x * other.x + self.y + other.y + self.z * other.z
+
+    def cross_product(self, other):
+        return Vector(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x
+        )
+
+    def magnitude(self):
+        return math.sqrt(
+            math.pow(self.x, 2) + math.pow(self.y, 2) + math.pow(self.z, 2)
+        )
+
+
+class Line:
+    def __init__(self, vector, point):
+        self.vector = vector
+        self.point = point
 
 
 class Scene:
@@ -50,12 +74,15 @@ class Scene:
 
     def _calculate_pixel_colour(self, x, y):
         screen_pixel_position = Point(x - self.screen.width / 2, y - self.screen.height / 2, self.screen.distance)
-        camera_to_pixel = Vector.from_points(self.camera_position, screen_pixel_position)
+        camera_to_pixel_vector = Vector.from_points(self.camera_position, screen_pixel_position)
+
+        camera_to_pixel = Line(camera_to_pixel_vector, self.camera_position)
 
         intersection_points = []
         for object in self.objects:
-            intersection_points_for_current = object.get_intersection_points(camera_to_pixel)
-            intersection_points.extend([(object, p) for p in intersection_points_for_current])
+            intersection_point_for_current = object.get_intersection_point(camera_to_pixel)
+            if intersection_point_for_current:
+                intersection_points.append((object, intersection_point_for_current))
 
         if intersection_points:
             closest_intersection = min(intersection_points, key=lambda p: p[1].distance_to(self.camera_position))
@@ -79,8 +106,34 @@ class Rectangle:
         self.p3 = p3
         self.colour = colour
 
-    def get_intersection_points(self, vector):
-        return [] #TODO
+        p1_p2 = self.p1.vector_to(self.p2)
+        p1_p3 = self.p1.vector_to(self.p3)
+        self.plane_normal = p1_p2.cross_product(p1_p3)
+
+    def get_intersection_point(self, line):
+        n_dot_u = self.plane_normal.dot_product(line.vector)
+        if abs(n_dot_u) < 0.01:
+            return None
+
+        w = Vector.from_points(self.p1, line.point)
+        s1 = -self.plane_normal.dot_product(w) / n_dot_u
+        intersection = Point(
+            w.x + s1 * line.vector.x + self.p1.x,
+            w.y + s1 * line.vector.y + self.p1.y,
+            w.z + s1 * line.vector.z + self.p1.z
+        )
+        a_m = Vector.from_points(self.p1, intersection)
+        a_b = Vector.from_points(self.p1, self.p2)
+        a_d = Vector.from_points(self.p1, self.p3)
+
+        am_dot_ab = a_m.dot_product(a_b)
+        ab_dot_ab = a_b.dot_product(a_b)
+        am_dot_ad = a_m.dot_product(a_d)
+        ad_dot_ad = a_d.dot_product(a_d)
+
+        if (0 < am_dot_ab < ab_dot_ab) or (0 < am_dot_ad < ad_dot_ad):
+            return intersection
+
 
 class Light:
     pass
@@ -100,7 +153,7 @@ COLOUR_RED = (255, 0, 0)
 
 scene = Scene(screen, camera_position, COLOUR_BLACK)
 
-rectange1 = Rectangle(Point(100, 100, 1500), Point(100, 200, 1500), Point(200, 200, 1500), COLOUR_RED)
+rectange1 = Rectangle(Point(-100, -100, 1500), Point(100, -100, 1500), Point(-100, 100, 1500), COLOUR_RED)
 scene.add_object(rectange1)
 
 scene.add_light(Light())
