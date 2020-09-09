0c73f17461214cef24895ffb613db36addad05d2
Rob Dawson <rob@codebox.org.uk>
2020-09-09 13:36:51 +0100

    np

--- np.py
+++ np.py
@@ -34,18 +34,18 @@ class Rect:
 
         w = np.subtract(self.p1, ray_origin)
 
-        s1 = np.where(n_dot_u, -self.normal.dot(w) / n_dot_u, None) # (6)
+        s1 = np.where(n_dot_u, -self.normal.dot(w) / n_dot_u, None)
 
-        intersection = np.where(s1 is None, np.zeros(3), w + np.transpose(s1 * rays_normalised) - self.p1) # (6,3)
+        intersection = np.where(s1 is None, np.zeros(3), w + np.transpose(s1 * rays_normalised) - self.p1)
 
-        a_m = np.subtract(intersection, self.p1) # (6,3)
-        a_b = np.repeat(np.subtract(self.p2, self.p1)[:,np.newaxis], len(a_m), axis=1).T # (6,3)
-        a_d = np.repeat(np.subtract(self.p3, self.p1)[:,np.newaxis], len(a_m), axis=1).T # (6,3)
+        a_m = np.subtract(intersection, self.p1)
+        a_b = np.repeat(np.subtract(self.p2, self.p1)[:,np.newaxis], len(a_m), axis=1).T
+        a_d = np.repeat(np.subtract(self.p3, self.p1)[:,np.newaxis], len(a_m), axis=1).T
 
-        am_dot_ab = np.sum(a_m * a_b, axis=1)   # (6)
-        ab_dot_ab = np.sum(a_b * a_b, axis=1)   # (6)
-        am_dot_ad = np.sum(a_m * a_d, axis=1)   # (6)
-        ad_dot_ad = np.sum(a_d * a_d, axis=1)   # (6)
+        am_dot_ab = np.sum(a_m * a_b, axis=1)
+        ab_dot_ab = np.sum(a_b * a_b, axis=1)
+        am_dot_ad = np.sum(a_m * a_d, axis=1)
+        ad_dot_ad = np.sum(a_d * a_d, axis=1)
 
         c1 = np.logical_and(0 < am_dot_ab, am_dot_ab < ab_dot_ab)
         c2 = np.logical_and(0 < am_dot_ad, am_dot_ad < ad_dot_ad)
@@ -59,13 +59,22 @@ rect2 = Rect((0,0,-1), (-1, -1, SCREEN_DISTANCE + 6), (-1, 1, SCREEN_DISTANCE + 
 objects = [rect1, rect2]
 rays = np.subtract(pixel_coords, camera_position)
 
+# intersections = [object.get_intersection_points(camera_position, rays) for object in objects]
+
 for object in objects:
     intersections = object.get_intersection_points(camera_position, rays)
-    print(intersections)
-
-
-
-
+    intersections_with_pixels = np.stack((intersections, pixel_coords), axis=1)
+    # print(intersections_with_pixels)
+    mask = intersections_with_pixels[:,0].any(axis=1)
+    print(mask)
+    hits = np.compress(mask, intersections_with_pixels, axis=0)
+    print(hits)
+    # print(np.extract(intersections_with_pixels[0] == 0, intersections_with_pixels))
+    # print(np.extract(np.compress(np.all(intersections_with_pixels[0] == 0), intersections_with_pixels, axis=0)))
+    # actual = np.extract(intersections != [0,0,0], intersections)
+    #
+    # intersections_to_light = np.subtract(intersections, light_position)
+    # print(intersections_to_light)
 
 
 # img = Image.fromarray(points, 'RGB')
